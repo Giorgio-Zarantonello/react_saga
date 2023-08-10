b62be1eb1daed5656ffce24bf65f38894205ab11
Zara <20912128+Giorgio-Zarantonello@users.noreply.github.com>
2023-08-10 15:52:27 +0200

    added model

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,53 +1,28 @@
-import React from "react";
-
-import { View, Text, Button } from "react-native";
-
-import { useDispatch, useSelector } from "react-redux";
-
-import { addTask, deleteTask } from "../slice/taskSlice";
-
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {addTask, deleteTask} from '../slice/taskSlice';
 const TaskList = () => {
-    
-        const dispatch = useDispatch();
-    
-        const tasks = useSelector((state) => state.task.tasks);
-    
-        const handleAddTask = () => {
-    
-            dispatch(addTask({ title: "Task 1", description: "Description 1" }));
-    
-        };
-    
-        const handleDeleteTask = (index) => {
-    
-            dispatch(deleteTask(index));
-    
-        };
-    
-        return (
-    
-            <View>
-    
-                <Button title="Add default Task" onPress={handleAddTask} />
-    
-                {tasks.map((task, index) => (
-    
-                    <View key={index}>
-    
-                        <Text>{task.title}</Text>
-    
-                        <Text>{task.description}</Text>
-    
-                        <Button title="Delete Task" onPress={() => handleDeleteTask(index)} />
-    
-                    </View>
-    
-                ))}
-    
-            </View>
-    
-        );
-    
-    };
+  const dispatch = useDispatch();
+  const tasks = useSelector(state => state.task.tasks);
+  const handleAddTask = () => {
+    dispatch(addTask({title: 'Task 1', description: 'Description 1'}));
+  };
+  const handleDeleteTask = index => {
+    dispatch(deleteTask(index));
+  };
+  return (
+    <View>
+      <Button title="Add default Task" onPress={handleAddTask} />
+      {tasks.map((task, index) => (
+        <View key={index}>
+          <Text>{task.title}</Text>
+          <Text>{task.description}</Text>
+          <Button title="Delete Task" onPress={() => handleDeleteTask(index)} />
+        </View>
+      ))}
+    </View>
+  );
+};
 
 export default TaskList;
---
+++ src/model/task.js
@@ -0,0 +1,9 @@
+class Task {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.id = Math.random().toString(36).substr(2, 9);
+    }
+}
+
+export default Task;
