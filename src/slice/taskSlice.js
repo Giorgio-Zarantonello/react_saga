d394b524cc910ef5cef86e35f08fe88553488ca8
Zara <20912128+Giorgio-Zarantonello@users.noreply.github.com>
2023-08-10 10:04:25 +0200

    first commit

---
+++ src/slice/taskSlice.js
@@ -0,0 +1,24 @@
+import {creasteSlice} from '@reduxjs/toolkit'
+
+// initial state
+const initialState = {
+    tasks : [],
+}
+
+// create slice
+const taskSlice = createSlice({
+    name : 'task',
+    initialState, 
+    reducers: {
+        addTask : (state, action) => {
+            state.tasks.push(action.payload);
+        },
+    },
+})
+
+// export actions
+export const {addTask} = taskSlice.actions;
+// export reducer
+export default taskSlice.reducer;
+
+
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import taskSlice from "../slice/taskSlice";
+
+const store = configureStore ({
+    reducer : {
+        task : taskSlice
+    }
+})
